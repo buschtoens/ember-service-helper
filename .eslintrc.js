cfb45b39b8cc3bbbf2525f4faa7b3f10b4dd86fd
Jan Buschtöns <jan@buschtoens.me>
2021-07-27 06:51:05 +0200

    chore: modernize

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,56 @@
+'use strict';
+
+// @TODO this can be removed once `trailingComma` is enabled upstream by default
+const prettierTrailingComma = {
+  'prettier/prettier': [
+    'error',
+    require('@clark/prettier-config/trailing-comma'),
+  ],
+};
+
+const ruleOverrides = {
+  ...prettierTrailingComma,
+};
+
 module.exports = {
   root: true,
-  extends: '@clark/node'
+  extends: ['@clark'],
+  parser: '@babel/eslint-parser',
+  parserOptions: {
+    requireConfigFile: false,
+  },
+  rules: {
+    ...ruleOverrides,
+  },
+  overrides: [
+    {
+      files: ['app/**/*.js'],
+      extends: '@clark/ember',
+      rules: { ...ruleOverrides },
+    },
+    {
+      files: ['addon-test-support/**/*.js', 'tests/**/*.js'],
+      excludedFiles: ['tests/dummy/config/**/*.js'],
+      extends: '@clark/ember/test',
+      rules: { ...ruleOverrides },
+    },
+    {
+      files: ['addon-test-support/**/*.js'],
+      rules: {
+        'qunit/no-commented-tests': 'off',
+      },
+    },
+    {
+      files: [
+        './*.js',
+        'blueprints/*/index.js',
+        'config/**/*.js',
+        'tests/dummy/config/**/*.js',
+        'lib/*/index.js',
+        'server/**/*.js',
+      ],
+      extends: '@clark/node',
+      rules: { ...ruleOverrides },
+    },
+  ],
 };
