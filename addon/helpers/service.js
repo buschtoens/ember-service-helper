c86bd9c61a7c2701d120d006ee9bef801a1f8d98
Jan Buschtöns <jan@buschtoens.me>
2021-07-27 08:13:25 +0200

    feat: auto-bound methods

--- addon/helpers/service.js
+++ addon/helpers/service.js
@@ -3,10 +3,22 @@ import Helper from '@ember/component/helper';
 import { assert } from '@ember/debug';
 
 export default class ServiceHelper extends Helper {
-  compute([serviceName]) {
+  compute([serviceName, methodName]) {
     const service = getOwner(this).lookup(`service:${serviceName}`);
     assert(`The service '${serviceName}' does not exist`, service);
 
-    return service;
+    if (!methodName) return service;
+    assert(
+      `If specified, the second parameter must be the name of a method. Received: '${methodName}'`,
+      typeof methodName === 'string' || typeof methodName === 'symbol',
+    );
+
+    const method = service[methodName];
+    assert(
+      `'${methodName}' is not a method on '${serviceName}'.`,
+      typeof method === 'function',
+    );
+
+    return method.bind(service);
   }
 }
