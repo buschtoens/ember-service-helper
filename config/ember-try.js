99a5676a6df3cb6b763c4c1e5a707c94c17559e1
Jan Buschtöns <jan@buschtoens.me>
2021-07-27 07:17:21 +0200

    chore(ci): update Ember LTS matrix

--- config/ember-try.js
+++ config/ember-try.js
@@ -6,22 +6,22 @@ module.exports = async function () {
   return {
     useYarn: true,
     scenarios: [
-      {
-        name: 'ember-lts-3.4',
-        npm: {
-          devDependencies: {
-            'ember-source': '~3.4.0',
-          },
-        },
-      },
-      {
-        name: 'ember-lts-3.8',
-        npm: {
-          devDependencies: {
-            'ember-source': '~3.8.0',
-          },
-        },
-      },
+      // {
+      //   name: 'ember-lts-3.4',
+      //   npm: {
+      //     devDependencies: {
+      //       'ember-source': '~3.4.0',
+      //     },
+      //   },
+      // },
+      // {
+      //   name: 'ember-lts-3.8',
+      //   npm: {
+      //     devDependencies: {
+      //       'ember-source': '~3.8.0',
+      //     },
+      //   },
+      // },
       {
         name: 'ember-lts-3.12',
         npm: {
@@ -35,6 +35,22 @@ module.exports = async function () {
         npm: {
           devDependencies: {
             'ember-source': '~3.16.0',
+          },
+        },
+      },
+      {
+        name: 'ember-lts-3.20',
+        npm: {
+          devDependencies: {
+            'ember-source': '~3.20.0',
+          },
+        },
+      },
+      {
+        name: 'ember-lts-3.24',
+        npm: {
+          devDependencies: {
+            'ember-source': '~3.24.0',
           },
         },
       },
